d5bf0bd2f37307ca639097d4dac407382c53b43b
sokxyy-ops <hurtdeathz@gmail.com>
2026-03-22 18:42:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 function sendJson(res, statusCode, data) {
   res.writeHead(statusCode, {
